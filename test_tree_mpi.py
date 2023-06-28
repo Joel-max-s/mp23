b4f736068d3ab36fb0baefd7c48adae34dd76315
Joel Seidel <Joel.Seidel01@gmail.com>
2023-06-28 22:56:00 +0200

    works even with odd number of cores

--- test_tree_mpi.py
+++ test_tree_mpi.py
@@ -19,20 +19,28 @@ if my_rank == 0:
 gens = math.ceil(math.log2(nprocs))
 for gen in range(gens):
     if 2**gen <= my_rank and my_rank < 2**(gen+1):
-        matrix = comm.recv(source=my_rank-(2**gen))
+        get_from = my_rank-(2**gen)
+        if get_from < nprocs:
+            matrix = comm.recv(source=get_from)
     if my_rank < 2**gen:
-        m = mx.split_matrix(matrix, 2)
-        matrix = m[0]
-        comm.send(m[1], my_rank+2**gen)
+        send_to = my_rank+2**gen
+        if send_to < nprocs:
+            m = mx.split_matrix(matrix, 2)
+            matrix = m[0]
+            comm.send(m[1], send_to)            
 
 r = mx.countIslands(matrix)
 
 for gen in range(gens-1, -1, -1):
     if 2**gen <= my_rank and my_rank < 2**(gen+1):
-        comm.send(r, my_rank-2**gen)
+        send_to = my_rank-2**gen
+        if send_to < nprocs:
+            comm.send(r, send_to)
     if my_rank < 2**gen:
-        r2 = comm.recv(source=my_rank+(2**gen))
-        r.merge(r2)
+        get_from = my_rank+(2**gen)
+        if get_from < nprocs:
+            r2 = comm.recv(source=get_from)
+            r.merge(r2)
 
 if my_rank == 0:
     end_p = time.time()
